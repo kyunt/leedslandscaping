fecaa935e83a97c98c99cc032f889de778ffc666
kyunt <galiyevdaniil@gmail.com>
2024-07-29 15:08:48 +0100

    Fixed mobile nav bar, added about page.

--- nav.js
+++ nav.js
@@ -1,22 +1,26 @@
-const primaryNav = document.querySelector(".primary-navigation");
-const navToggle = document.querySelector(".nav-toggle-icon");
-const heroMain = document.querySelector(".hero");
-const headerContainer = document.querySelector(".header-cont");
-const headerContainerOther = document.querySelector(".header-cont-other");
+document.addEventListener("DOMContentLoaded", () => {
+  const primaryNav = document.querySelector(".primary-navigation");
+  const navToggle = document.querySelector(".nav-toggle-icon");
+  const heroMain = document.querySelector(".hero");
+  const headerContainer = document.querySelector(".header-cont");
 
-navToggle.addEventListener("click", () => { 
-  const visibility = primaryNav.getAttribute("data-visible");
-  
-  if (visibility === "false") {
-    primaryNav.setAttribute("data-visible", true);
-    navToggle.setAttribute("aria-expanded", true);
-    heroMain.setAttribute("data-nav-open", true);
-    headerContainer.setAttribute("data-nav-open", true);
-    headerContainerOther.setAttribute("data-nav-open", true);
-  } else if (visibility === "true") {
-    primaryNav.setAttribute("data-visible", false);
-    navToggle.setAttribute("aria-expanded", false);
-    heroMain.setAttribute("data-nav-open", false);
-    headerContainer.setAttribute("data-nav-open", false);}
-    headerContainerOther.setAttribute("data-nav-open", false);
+  const heroSub = document.querySelector("#sub-hero");
+
+  navToggle.addEventListener("click", () => { 
+    const visibility = primaryNav.getAttribute("data-visible");
+    
+    if (visibility === "false") {
+      primaryNav.setAttribute("data-visible", true);
+      navToggle.setAttribute("aria-expanded", true);
+      heroMain.setAttribute("data-nav-open", true);
+      headerContainer.setAttribute("data-nav-open", true);
+      heroSub.setAttribute("data-nav-open", true);
+    } else if (visibility === "true") {
+      primaryNav.setAttribute("data-visible", false);
+      navToggle.setAttribute("aria-expanded", false);
+      heroMain.setAttribute("data-nav-open", false);
+      headerContainer.setAttribute("data-nav-open", false);
+      heroSub.setAttribute("data-nav-open", false);
+    }
+  });
 });
